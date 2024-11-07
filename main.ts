60bc5a684b90dc4829ab14d4171555dab9d5566d
Moiz Qureshi <m.moiz782008@gmail.com>
2024-11-07 23:02:36 +0500

    commint
    change

--- main.ts
+++ main.ts
@@ -1,23 +1,4 @@
 // LISTING ELEMENT
 document.getElementById('resumeForm')?.addEventListener('submit', function(event){
     event.preventDefault();
-
-    // TYPE ASSERTION
-
-    const profilePictureInput = document.getElementById('profilePicture') as HTMLInputElement;
-
-    if(profilePictureInput){
-        
-        // Picture element
-        const profilePicturefile = profilePictureInput.files?.[0]
-        const profilePictureURL = profilePicturefile ? URL.createObjectURL(profilePicturefile) : "" ;
-
-
-
-         // CREATE RESUME OUTPUT
-         const resumeOutPut = `
-         <h2>Resume</h2>
-         ${profilePictureURL ? `<img src="${profilePictureURL}" alt="Profile Picture" class="profilePicture">` : '' }
-        `;
-    }
 });
